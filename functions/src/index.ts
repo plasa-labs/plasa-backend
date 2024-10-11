ec7ef386096c850a5bbcb210a1cf0b4d7e0b1fad
nicoacosta.eth <nicoacosta8@gmail.com>
2024-10-11 02:46:19 -0300

    functions: deployed eip712 signature for follower since

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -14,6 +14,7 @@ import { getFirestore } from 'firebase-admin/firestore'
 import { initializeApp } from 'firebase-admin/app'
 
 import { signInstagramAccountOwnership } from './eip712/account-ownership'
+import { signInstagramFollowerSince } from './eip712/follower-since'
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -21,10 +22,14 @@ import { signInstagramAccountOwnership } from './eip712/account-ownership'
 initializeApp()
 const db = getFirestore()
 
+// Testing functions
+
 export const helloWorld = onRequest((request, response) => {
 	logger.info('Hello logs!', { structuredData: true })
 	response.send('Hello from Firebase!')
 })
+
+// Testing firestore read
 
 export const checkFollowerSince = onRequest(async (request, response) => {
 	try {
@@ -52,6 +57,8 @@ export const checkFollowerSince = onRequest(async (request, response) => {
 	}
 })
 
+// Testing EIP712 signatures
+
 export const instagramAccountOwnershipSignature = onRequest(async (request, response) => {
 	const username = request.query.username as string
 	const recipient = request.query.recipient as string
@@ -61,5 +68,21 @@ export const instagramAccountOwnershipSignature = onRequest(async (request, resp
 	}
 
 	const signature = await signInstagramAccountOwnership(username, recipient)
+	response.json({ signature })
+})
+
+export const instagramFollowerSinceSignature = onRequest(async (request, response) => {
+	const { followed, follower, since, recipient } = request.query
+
+	if (!followed || !follower || !since || !recipient) {
+		throw new HttpsError('invalid-argument', 'Missing followed, follower, since, or recipient!')
+	}
+
+	const signature = await signInstagramFollowerSince(
+		followed as string,
+		follower as string,
+		Number(since),
+		recipient as string
+	)
 	response.json({ signature })
 })
