d7cd43de78921e9a1e639bead251751b6ca63f1c
nicoacosta.eth <nicoacosta8@gmail.com>
2024-10-11 02:05:58 -0300

    functions: add eip abstract implementation

---
+++ functions/src/eip712/eip712.ts
@@ -0,0 +1,44 @@
+import { ethers } from 'ethers'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const signerPrivateKey = process.env.EIP712_SIGNER_PRIVATE_KEY!
+console.log('signerPrivateKey', signerPrivateKey)
+const chainId = parseInt(process.env.CHAIN_ID!)
+
+interface EIP712Domain {
+	name: string
+	version: string
+	chainId: number
+	verifyingContract: string
+}
+
+class EIP712Signer {
+	private domain: EIP712Domain
+	private signer: ethers.Wallet
+
+	constructor(verifyingContract: string) {
+		this.domain = {
+			name: 'Plasa Stamps',
+			version: '0.1.0',
+			chainId: chainId,
+			verifyingContract: verifyingContract
+		}
+		this.signer = new ethers.Wallet(signerPrivateKey)
+	}
+
+	async signTypedData(
+		types: Record<string, Array<{ name: string; type: string }>>,
+		message: Record<string, unknown>
+	): Promise<string> {
+		const signature = await this.signer.signTypedData(this.domain, types, message)
+		return signature
+	}
+
+	get signerAddress(): string {
+		return this.signer.address
+	}
+}
+
+export { EIP712Signer }
