ce374f5f7be60f43c67845e2b66ddf95158ca4e6
nicoacosta.eth <nicoacosta8@gmail.com>
2024-10-29 01:03:54 -0300

    feat(hono): implement hono for requests

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,45 @@
 import { initializeApp } from 'firebase-admin/app'
+import { Hono } from 'hono'
+import { onRequest, Request as FirebaseRequest } from 'firebase-functions/v2/https'
+import userRouter from './user/controller'
 
 /** Initialize Firebase app */
 initializeApp()
 
-import * as testFunctions from './test'
-import * as prodFunctions from './prod'
+/** Create a new Hono app */
+const app = new Hono()
 
-/**
- * Export test functions
- * @namespace testFunctions
- */
-export const { helloWorld } = testFunctions
+// app.use('*', async (c, next) => {
+// 	console.log(`Incoming request: ${c.req.method} ${c.req.path}`)
+// 	await next()
+// })
 
-/**
- * Export production functions
- * @namespace prodFunctions
- */
-export const { getUserData, linkInstagram, getInstagramUsername } = prodFunctions
+/** Define a simple route */
+app.get('/', (c) => c.text('Hono!'))
+
+/** Use the userRouter for /user routes */
+app.route('/user', userRouter)
+
+// app.onError((error, c) => {
+// 	console.error('Error occurred:', error)
+// 	return c.text('Internal Server Error', 500)
+// })
+
+/** Function to adapt Firebase request to Hono request */
+const adaptRequest = (req: FirebaseRequest) => {
+	return new Request(req.url, {
+		method: req.method,
+		headers: req.headers as HeadersInit,
+		body: req.body
+	})
+}
+
+/** Export the Hono app as a Firebase Function */
+export const api = onRequest(async (req, res) => {
+	const honoReq = adaptRequest(req)
+
+	const honoResponse = await app.fetch(honoReq)
+
+	// Set the status and send the response back
+	res.status(honoResponse.status).send(await honoResponse.text())
+})
