9fdec48d5fdb5f1610a2d51fd7b8cc1da187ba96
nicoacosta.eth <nicoacosta8@gmail.com>
2024-10-29 00:38:10 -0300

    fix: remove old files

--- functions/src/test.ts
+++
@@ -1,8 +0,0 @@
-import { onRequest } from 'firebase-functions/v2/https'
-import * as logger from 'firebase-functions/logger'
-
-// Example function (can be removed in production)
-export const helloWorld = onRequest((request, response) => {
-	logger.info('Hello logs!', { structuredData: true })
-	response.send('Hello from Firebase!')
-})
