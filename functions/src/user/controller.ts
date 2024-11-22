fb9b1a329c5b39028679f5a54095e1fd6016558a
nicoacosta.eth <nicoacosta8@gmail.com>
2024-11-22 02:14:37 -0300

    fix: remove deprecated instagram link

--- functions/src/user/controller.ts
+++ functions/src/user/controller.ts
@@ -8,58 +8,23 @@ const userService = new UserService()
 /**
  * Retrieves full data for a user.
  */
-export async function getUserFullData(req: express.Request, res: express.Response) {
-	console.log('getUserFullData')
+export async function getUserData(req: express.Request, res: express.Response) {
+	console.log('getUserData')
 	try {
 		const userId = req.params.id
 		console.log('userId', userId)
 
-		const userFullData: UserResponse = await userService.getUserFullData(userId)
-		console.log(userFullData)
+		const userData: UserResponse = await userService.getUserData(userId)
+		console.log(userData)
 
-		return res.json(userFullData)
+		return res.json(userData)
 	} catch (error) {
 		console.error(error)
 		return res.status(500).json({ message: 'Failed to retrieve user data', error })
 	}
 }
 
-/**
- * Sets the Instagram username for a user.
- */
-export async function setUserInstagram(req: express.Request, res: express.Response) {
-	try {
-		console.log('setUserInstagram')
-
-		const userId = req.params.id
-		console.log('userId', userId)
-
-		if (!userId) {
-			return res.status(400).json({ message: 'User ID is required' })
-		}
-
-		const { username } = req.body
-		console.log('username', username)
-
-		if (!username) {
-			return res.status(400).json({ message: 'Instagram username is required' })
-		}
-
-		const updatedUserFullData: UserResponse | null = await userService.setUserInstagram(
-			userId,
-			username
-		)
-
-		console.log(updatedUserFullData)
-		return res.json(updatedUserFullData)
-	} catch (error) {
-		console.error(error)
-		return res.status(500).json({ message: 'Failed to set Instagram username', error })
-	}
-}
-
 // Route definitions using the controller functions
-userRouter.get('/:id', getUserFullData)
-userRouter.post('/:id/instagram', setUserInstagram)
+userRouter.get('/:id', getUserData)
 
 export default userRouter
